5246aea5452d2a2206ef876e9b0d009b79e217a6
leloeduk <leloeduk2025@gmail.com>
2025-10-18 21:33:08 +0100

    feat: work on user_page.dart

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -49,31 +49,39 @@ class UserPage extends StatelessWidget {
                   topRight: Radius.circular(20),
                 ),
               ),
-              child: ListView.builder(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                ),
                 itemCount: users.length,
                 itemBuilder: (context, index) {
-                  final currentUserIndex = users[index];
-                  return Card(
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(currentUserIndex.image),
-                      ),
-
-                      title: Text(
-                        currentUserIndex.name,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                  final currentIndexUser = users[index];
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 120,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(currentIndexUser.image),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
-                      ),
-                      subtitle: Text(
-                        currentUserIndex.email,
-                        style: TextStyle(color: Colors.grey),
-                      ),
-
-                      trailing: currentUserIndex.isOnline
-                          ? Text("En ligne 🟢")
-                          : Text("Hors ligne 🔴"),
+                        Center(
+                          child: Text(
+                            currentIndexUser.name,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
                     ),
                   );
                 },
